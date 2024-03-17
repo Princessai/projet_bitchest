4e116d6134273430fa4fc85c4dd68c797c948a2e
Oumou Coulibaly <coumou100@gmail.com>
2024-03-17 15:42:53 +0000

    ajustement page

--- site/app/Http/Controllers/CustomerController.php
+++ site/app/Http/Controllers/CustomerController.php
@@ -5,59 +5,65 @@ namespace App\Http\Controllers;
 use App\Models\Customer;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+
 class CustomerController extends Controller
 {
-    
-
-       public function list() : view {
-        // $customer = Customer::paginate(4);
-         $customer = Customer::all();
-        return view('pages.admin.customerAdmin',compact('customer'));
-       }
 
 
-  public function update($id){
+  public function list(): view
+  {
+    // $customer = Customer::paginate(4);
+    $customer = Customer::all();
+    return view('pages.admin.customerAdmin', compact('customer'));
+  }
+
+
+  public function update($id)
+  {
     $customer = Customer::find($id);
     return view('pages.admin.customerUpdate', compact(('customer')));
   }
 
-public function update_traitement(Request $request)
-{
+  public function update_traitement(Request $request)
+  {
     $request->validate([
-            'firstname' => 'required',
-            'lastname' => 'required',
-            'age' => 'required',
-            'email' => 'required',
-           
-        ]);
+      'firstname' => 'required|string',
+      'lastname' => 'required|string',
+      'age' => 'required|numeric',
+      'email' => 'required|email:rfc,dns',
 
-        $customer =  Customer::find($request->id);
-        $customer->firstname = $request->firstname;
-        $customer->lastname = $request->lastname;
-        $customer->age = $request->age;
-        $customer->email = $request->email;
-        $customer->update();
+    ]);
 
-        return redirect('/customer');
+    $customer =  Customer::find($request->id);
+    $customer->firstname = $request->firstname;
+    $customer->lastname = $request->lastname;
+    $customer->age = $request->age;
+    $customer->email = $request->email;
+    $customer->update();
 
+    return redirect('/customer');
+  }
 
-}
-
-public function delete($id){
+  public function delete($id)
+  {
     $customer = Customer::find($id);
     $customer->delete();
 
     return redirect('/customer');
-}
+  }
 
- public function add(){
+  public function add()
+  {
     return view('pages.admin.customerAdd');
- }
+  }
 
 
- public function view($id) {
+  public function view($id)
+  {
     $customer = Customer::find($id);
     return view('pages.admin.customerView', compact('customer'));
- }
+  }
+
+  
 
 }
--- site/resources/views/pages/admin/customerAdd.blade.php
+++ site/resources/views/pages/admin/customerAdd.blade.php
@@ -1,30 +1,35 @@
 @extends('layouts.adminDashboard');
 @section('bodycontent')
-<div class="container w-50
+    <div class="container w-50
  mt-3 profile shadow p-3 mb-5 rounded rounded text-center">
-  <div class="row">
-    <div class="col-md-12 d-flex flex-column  mt-3  ">
-        <h2 class="text-light">Add</h2>
-    <form method="POST" action="/add/traitement">
-        @csrf
+        <div class="row">
+            <div class="col-md-12 d-flex flex-column  mt-3  ">
+                <h2 class="text-light">Add</h2>
+                <form method="POST" action="/add/traitement">
+                    @csrf
 
-    <h6 class="text-light">Customer </h6>
-    <input type="text" id="TextInput" name="id" style="display: none;" class="form-control"  value="">
-    <div class="mb-3">
-      <input type="text" id="TextInput" name="firstname" class="form-control" placeholder="firstname" value="">
+                    <h6 class="text-light">Customer </h6>
+                    <input type="text" id="TextInput" name="id" style="display: none;" class="form-control"
+                        value="">
+                    <div class="mb-3">
+                        <input type="text" id="TextInput" name="firstname" class="form-control" placeholder="firstname"
+                            value="">
+                    </div>
+                    <div class="mb-3">
+                        <input type="text" id="disabledTextInput" name="lastname" class="form-control"
+                            placeholder="lastname" value="">
+                    </div>
+                    <div class="mb-3">
+                        <input type="text" id="disabledTextInput" name="age" class="form-control" placeholder="age"
+                            value="">
+                    </div>
+                    <div class="mb-3">
+                        <input type="text" id="disabledTextInput" name="email" class="form-control" placeholder="email"
+                            value="">
+                    </div>
+                    <input type="submit" id="TextInput" class="form-control align-self-center" value="create">
+                </form>
+            </div>
+        </div>
     </div>
-    <div class="mb-3">
-      <input type="text" id="disabledTextInput" name="lastname" class="form-control" placeholder="lastname" value="">
-    </div>
-    <div class="mb-3">
-      <input type="text" id="disabledTextInput" name="age" class="form-control" placeholder="age" value="">
-    </div>
-    <div class="mb-3">
-      <input type="text" id="disabledTextInput" name="email" class="form-control" placeholder="email" value="">
-    </div>
-      <input type="submit" id="TextInput" class="form-control align-self-center"  value="create">
-</form>
-    </div>
-  </div>
-</div>
 @endsection
